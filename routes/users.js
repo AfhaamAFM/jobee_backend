eb04fac757a84771df859475bc4b7976becd8897
Afhaam k <afhaamcoc@gmail.com>
2022-01-18 13:10:27 +0530

    ok da

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,10 @@ router.post('/verifyOtp',verifyOtp)
 router.post('/testOtpSend',testSend)
 router.post('/testVerify',testVerify)
 router.post('/testForFazil',(req,res)=>{
-
+const {name}= req.body
   res.json({
-    fazil:"ooomb myre.neeeeeti"
+
+    fazil:"ooomb myre.neeeeeti"+name
   })
 
 })
